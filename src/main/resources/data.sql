867164b5f522109cd115a8fa3027dfc8d5547560
Gumaro Monroy Vazquez <maromvz@gmail.com>
2026-01-14 00:55:15 -0600

    Add product to initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,5 @@ DELETE FROM user_product;
 
 INSERT INTO product (name, price, created_at) VALUES
     ('Masaje Reductor', 200, CURRENT_TIMESTAMP),
-    ('Masaje Relajante', 300, CURRENT_TIMESTAMP);
+    ('Masaje Relajante', 300, CURRENT_TIMESTAMP),
+    ('Masaje Facial', 200, CURRENT_TIMESTAMP);
